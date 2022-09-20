b377ecb7f43901b5ad5528ad310c2f83ab83530b
Sonia Parmar <soniaparmar24@gmail.com>
2022-09-20 20:49:16 +0200

    modify the plot title

--- ann_regression.py
+++ ann_regression.py
@@ -8,9 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-from configfile import input_csv
-
-dataset = pd.read_csv("input_csv")
+dataset = pd.read_csv("amptsm.csv")
 X = dataset.iloc[:, :15].values
 y = dataset.iloc[:, -29].values
 
@@ -22,7 +20,7 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test  = sc.transform(X_test)
 
-''' build DNN model with 3 dense layers '''
+''' build the Sequential model'''
 ann = tf.keras.models.Sequential()
 ann.add(tf.keras.layers.Dense(units = 15, activation = "relu", input_shape = [15]))
 ann.add(tf.keras.layers.Dense(units = 128, activation = "relu"))
@@ -30,7 +28,6 @@ ann.add(tf.keras.layers.Dense(units = 128, activation = "relu"))
 ann.add(tf.keras.layers.Dense(units = 128, activation = "relu"))
 ann.add(tf.keras.layers.Dense(units = 1))
 
-''' train the model '''
 ann.compile(optimizer = "adam", loss = "mean_squared_error")
 history = ann.fit(
             X_train, y_train,
@@ -38,21 +35,19 @@ history = ann.fit(
             batch_size = 32,
             epochs = 50)
 
-
 history_df = pd.DataFrame(history.history)
 #print(history_df['loss'], history_df['val_loss'])
 
-# prediction on the test data set 
+# prediction on the test data set
 y_pred = ann.predict(X_test)
 #np.set_printoptions(precision = 2)
 #print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
-
 
 # data visualization (plotting loss functions)
 plt.plot(history_df['loss'])
 y_pred = ann.predict(X_test)
 plt.plot(history_df['val_loss'])
-plt.title("Loss function (Adam)")
+plt.title("Loss function (Train & Test set)")
 plt.xlabel("epochs")
 plt.ylabel("loss")
 plt.show()
