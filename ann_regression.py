a4ee0e6c448e45e2e9f04e4adbba01a6873bd06a
Sonia Parmar <soniaparmar24@gmail.com>
2023-03-24 14:45:31 +0100

    Add if __name__ == "__main__":

--- ann_regression.py
+++ ann_regression.py
@@ -1,56 +1,60 @@
-#######################################################################
-######## ANN regression with dense neural network layers (DNN) ########
-#######################################################################
+""" ANN regression with dense neural network layers (DNN) """
+#############################################################
 
-# import libraries
 import pandas as pd
-import numpy as np
+# import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
-
-from config import inputfile
-dataset = pd.read_csv(inputfile)
-
-X = dataset.iloc[:, :15].values
-y = dataset.iloc[:, -29].values
-
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-
 from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test  = sc.transform(X_test)
+from config import inputfile
 
-''' build the Sequential model'''
-ann = tf.keras.models.Sequential()
-ann.add(tf.keras.layers.Dense(units = 15, activation = "relu", input_shape = [15]))
-ann.add(tf.keras.layers.Dense(units = 128, activation = "relu"))
-ann.add(tf.keras.layers.Dense(units = 128, activation = "relu"))
-ann.add(tf.keras.layers.Dense(units = 128, activation = "relu"))
-ann.add(tf.keras.layers.Dense(units = 1))
 
-ann.compile(optimizer = "adam", loss = "mean_squared_error")
-history = ann.fit(
-            X_train, y_train,
-            validation_data = (X_test, y_test),
-            batch_size = 32,
-            epochs = 50)
+def main():
+    """ predict correlations with ann dense layers """
+    dataset = pd.read_csv(inputfile)
 
-history_df = pd.DataFrame(history.history)
-#print(history_df['loss'], history_df['val_loss'])
+    X = dataset.iloc[:, :15].values
+    y = dataset.iloc[:, -29].values
 
-# prediction on the test data set
-y_pred = ann.predict(X_test)
-#np.set_printoptions(precision = 2)
-#print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=0
+    )
 
-# data visualization (plotting loss functions)
-plt.plot(history_df['loss'])
-y_pred = ann.predict(X_test)
-plt.plot(history_df['val_loss'])
-plt.title("Loss function (Train & Test set)")
-plt.xlabel("epochs")
-plt.ylabel("loss")
-plt.show()
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
 
+    """ build the Sequential model"""
+    ann = tf.keras.models.Sequential()
+    ann.add(tf.keras.layers.Dense(units=15, activation="relu", input_shape=[15]))
+    ann.add(tf.keras.layers.Dense(units=128, activation="relu"))
+    ann.add(tf.keras.layers.Dense(units=128, activation="relu"))
+    ann.add(tf.keras.layers.Dense(units=128, activation="relu"))
+    ann.add(tf.keras.layers.Dense(units=1))
+
+    ann.compile(optimizer="adam", loss="mean_squared_error")
+    history = ann.fit(
+        X_train, y_train, validation_data=(X_test, y_test), batch_size=32, epochs=10
+    )
+
+    history_df = pd.DataFrame(history.history)
+    # print(history_df['loss'], history_df['val_loss'])
+
+    # prediction on the test data set
+    y_pred = ann.predict(X_test)
+    # np.set_printoptions(precision = 2)
+    # print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+    # data visualization (plotting loss functions)
+    plt.plot(history_df["loss"])
+    y_pred = ann.predict(X_test)
+    plt.plot(history_df["val_loss"])
+    plt.title("Loss function (Train & Test set)")
+    plt.xlabel("epochs")
+    plt.ylabel("loss")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
